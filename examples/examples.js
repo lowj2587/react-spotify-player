9d7d49001209d6d891d3d8befc9b710c75059c7c
lowj2587 <102533264+lowj2587@users.noreply.github.com>
2024-10-17 10:43:43 -0400

    Update examples.js

--- examples/examples.js
+++ examples/examples.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import SpotifyPlayer from '../dist/SpotifyPlayer.js';
 
 ReactDOM.render(
-  <SpotifyPlayer uri="spotify:album:1TIUsv8qmYLpBEhvmBmyBk" size={{width: 800, height: 500}} theme="white" view="list" />,
+  <SpotifyPlayer uri="spotify:playlist:23PI1CSy11a11vbBy1pwvg" size={{width: 800, height: 500}} theme="white" view="list" />,
   document.querySelector('.player')
 );
